336d99e6c8938087d21458d5f03632f4419ab544
andrewmelis <amelis@pathf.com>
2014-03-02 21:16:27 -0600

    first passing spec and feature

--- the_rspec_book/codebreaker/spec/codebreaker/game_spec.rb
+++ the_rspec_book/codebreaker/spec/codebreaker/game_spec.rb
@@ -3,27 +3,16 @@ require 'spec_helper'
 module Codebreaker
   describe Game do
     describe "#start" do
+      let(:output) { double('output').as_null_object }
+      let(:game)   { Game.new(output) }
+
       it "sends a welcome message" do
-	#setup
-	output = double('output').as_null_object
-	game = Game.new(output)
-
-	#set expectations
 	output.should_receive(:puts).with('Welcome to Codebreaker!')
-
-	#call behavior
 	game.start
       end
 
       it "prompts for the first guess" do
-	#setup objects
-	output = double('output').as_null_object
-	game = Game.new(output)
-
-	#setup expectations
 	output.should_receive(:puts).with('Enter guess:')
-
-	#call behavior
 	game.start
       end
     end
