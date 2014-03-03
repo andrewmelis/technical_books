954bea0af055356eb05f2bd8f14edc86b7950860
andrewmelis <amelis@pathf.com>
2014-03-02 21:07:26 -0600

    initial commit. finished 5.3 in rspec book

---
+++ the_rspec_book/ch1/movielist_spec.rb
@@ -0,0 +1,10 @@
+require_relative 'movielist'
+
+describe MovieList do
+  context "when first created" do
+    it "is empty" do
+      movie_list = MovieList.new
+      movie_list.should be_empty
+    end
+  end
+end
